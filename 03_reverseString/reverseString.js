4d4524c838a98714f84d27216d7b2c723093a47b
Javon_F1996 <javonf1996@gmail.com>
2023-02-02 23:39:12 -0400

    completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(wrdArray) {
+    const txtAsArray = wrdArray.split("");
+    txtAsArray.reverse();
+    return txtAsArray.join("");    
 };
 
 // Do not edit below this line
