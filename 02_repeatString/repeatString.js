5a257286336fac5f846e92458ab9ddfc24879b48
Javon_F1996 <javonf1996@gmail.com>
2023-02-02 22:17:43 -0400

    completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, repNum) {
+    let newWord = '';
+    for (let i = 0; i < repNum; i++) {
+        newWord += word;
+    }
+    return newWord
 };
-
 // Do not edit below this line
 module.exports = repeatString;
